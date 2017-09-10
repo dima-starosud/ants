ad98e78a18e812f561ac79a20efb94c2904187f5
Dmytro Starosud <d.starosud@gmail.com>
2017-09-10 16:31:16 +0300

    explicit borders

--- src/main/scala/ants/Matrix.scala
+++ src/main/scala/ants/Matrix.scala
@@ -1,7 +1,7 @@
 package ants
 
 import akka.typed.scaladsl.Actor
-import akka.typed.{ActorRef, Behavior}
+import akka.typed.{ActorRef, ActorSystem, Behavior}
 
 object Matrix {
   final case class Pos(x: Int, y: Int) {
@@ -18,32 +18,35 @@ object Matrix {
   final case object Accepted extends PassResult
   final case object Denied extends PassResult
 
-  final case class Active(pos: Pos, cells: Pos => ActorRef[Pass]) {
+  def available(
+    visitedCallback: () => Unit,
+    cells: Direction => ActorRef[Pass],
+  ): Behavior[Pass] = Actor.immutable {
+    (ctx, e) =>
+      e.from ! Accepted
+      visitedCallback()
+      ctx.spawnAnonymous(busy(e.dir, cells))
+      Visited
+  }
 
-    lazy val Available: Behavior[Pass] = Actor.immutable {
-      (ctx, e) =>
-        e.from ! Accepted
-        println(pos) // TODO delegate
-        ctx.spawnAnonymous(Busy(e.dir))
-        Visited
-    }
-
-    def Busy(dir: Direction): Behavior[PassResult] = {
-      lazy val retry: Seq[Direction] => Behavior[PassResult] = {
-        case Seq() =>
-          Actor.stopped
-        case dir +: dirs =>
-          Actor.deferred { ctx =>
-            cells(pos + dir) ! Pass(ctx.self, dir)
-            Actor.immutable {
-              case (_, Accepted) => retry(Seq.empty)
-              case (_, Denied) => retry(dirs)
-            }
+  def busy(
+    dir: Direction,
+    cells: Direction => ActorRef[Pass],
+  ): Behavior[PassResult] = {
+    lazy val retry: Seq[Direction] => Behavior[PassResult] = {
+      case Seq() =>
+        Actor.stopped
+      case dir +: dirs =>
+        Actor.deferred { ctx =>
+          cells(dir) ! Pass(ctx.self, dir)
+          Actor.immutable {
+            case (_, Accepted) => retry(Seq.empty)
+            case (_, Denied) => retry(dirs)
           }
-      }
-
-      retry(Seq.iterate(dir, 4)(_.rotate))
+        }
     }
+
+    retry(Seq.iterate(dir, 4)(_.rotate))
   }
 
   lazy val Visited: Behavior[Pass] = Actor.immutable {
@@ -52,22 +55,32 @@ object Matrix {
       Actor.same
   }
 
-  def circle(
-    xs: Int, ys: Int,
-  ): Behavior[Nothing] = Actor.deferred[Nothing] { ctx =>
-
+  def circle(xs: Int, ys: Int): Behavior[Nothing] = Actor.deferred[Nothing] { ctx =>
     lazy val cells: Map[Pos, ActorRef[Pass]] = {
-      val visited = ctx.spawnAnonymous(Visited)
-      for {
-        x <- Seq.range(1, xs + 1)
-        y <- Seq.range(1, ys + 1)
-      } yield {
-        val pos = Pos(x, y)
-        val ref = ctx.spawnAnonymous(
-          Active(pos, cells.getOrElse(_, visited)).Available)
-        pos -> ref
-      }
-    }.toMap
+      val xss = Seq.range(1, xs + 1)
+      val yss = Seq.range(1, ys + 1)
+
+      val active =
+        for {
+          x <- xss
+          y <- yss
+        } yield {
+          val pos = Pos(x, y)
+          val ref = ctx.spawnAnonymous(
+            available(() => println(pos), d => cells(pos + d)))
+          pos -> ref
+        }
+
+      val sc = Stream.continually[Int] _
+      val border =
+        for {
+          (x, y) <- xss.zip(sc(0)) ++ xss.zip(sc(ys + 1)) ++ sc(0).zip(yss) ++ sc(xs + 1).zip(yss)
+        } yield {
+          Pos(x, y) -> ctx.spawnAnonymous(Visited)
+        }
+
+      (active ++ border).toMap
+    }
 
     val initiator = ctx.spawnAnonymous(Actor.immutable[PassResult] {
       (_, e) =>
@@ -84,4 +97,7 @@ object Matrix {
 
     Actor.empty
   }
+
+  def runCircle(xs: Int, ys: Int): ActorSystem[Nothing] =
+    ActorSystem[Nothing](Matrix.circle(xs, ys), "Matrix-circle")
 }
